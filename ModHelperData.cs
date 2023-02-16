4d3f5f6030f44db72f2d1dcc456be8cbb1584fa5
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-02-16 17:02:45 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@
         public const string Version = "1.0.0";
         public const string Name = "Double Projectiles";
         public const string Description =
-            "This mod makes all upgrades double the towers projectiles. <br><br>This mod was originally made by Turkey, I am just updating it for newer versions.";
+            "This mod makes all upgrades double the towers projectiles. <br><br>This mod was originally made by Turkey, I have just updated it for newer versions of BTD6.";
 
         public const string RepoOwner = "Jonyboylovespie";
         public const string RepoName = "DoubleProjectiles";
